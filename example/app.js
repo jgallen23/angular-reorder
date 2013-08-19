96a6943560b75cbf7205003f4d9001d527ac9b87
Troy Whiteley <troy@somanyscientists.com>
2013-08-19 11:33:59 -0700

    Stores order.

--- example/app.js
+++ example/app.js
@@ -1,9 +1,9 @@
 angular.module('example-app', ['reorder']);
 
 var MainController = function($scope) {
-  $scope.order = [];
+  $scope.itemList = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7'];
 
-   $scope.$watch('order', function() {
+   $scope.$watch('itemList', function() {
       console.log('order changed', arguments);
     }, true);
 };
