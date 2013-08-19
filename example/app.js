f492c38fd7a7441c22ad97b656ec856ce4159ea8
Troy Whiteley <troy@somanyscientists.com>
2013-08-19 10:37:02 -0700

    script fail

--- example/app.js
+++ example/app.js
@@ -1,5 +1,9 @@
 angular.module('example-app', ['reorder']);
 
 var MainController = function($scope) {
+  $scope.order = [];
 
+   $scope.$watch('order', function() {
+      console.log('order changed', arguments);
+    }, true);
 };
